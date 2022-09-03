ca0ba6ab927b3f739258c3d9dcf1a4b18bcae1c3
ahm-rubayed <rubayedahmed04@gmail.com>
2022-09-03 07:11:00 +0600

    Catagory added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log("Hello World")
+const catagory = () => {
+    fetch(`https://openapi.programming-hero.com/api/news/categories`)
+    .then (res => res.json())
+    .then (data => catagoryContainer(data.data.news_category))
+}
+
+const catagoryContainer = (catagoryData) => {
+    const catagoryCon = document.getElementById('catagory-container');
+    catagoryData.forEach(data => {
+        console.log(data);
+        const li = document.createElement('li');
+        li.innerHTML = `
+        <li class="my-1 lg:my-0 cursor-pointer">${data.category_name}</li>
+        `
+
+        catagoryCon.appendChild(li);
+    })
+}
+
+catagory();
